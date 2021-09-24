ff320dac17f7a3cb5c39a54052e71e42d406de52
Abdullah Furkan Titiz <65949007+afurkantitiz@users.noreply.github.com>
2021-09-24 11:53:16 +0300

    Created navigation graph and bottom navigation bar

--- app/src/main/java/com/afurkantitiz/newsapp/ui/MainActivity.kt
+++ app/src/main/java/com/afurkantitiz/newsapp/ui/MainActivity.kt
@@ -2,11 +2,28 @@ package com.afurkantitiz.newsapp.ui
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI
 import com.afurkantitiz.newsapp.R
+import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var navController: NavController
+    private lateinit var bottomNavigationView: BottomNavigationView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        bottomNavigationView = findViewById(R.id.bottomNavigationView)
+        navController = Navigation.findNavController(this, R.id.navHostFragment)
+
+        NavigationUI.setupWithNavController(bottomNavigationView, navController)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(navController, null)
     }
 }
