1b452e2cc4ca797c31c2101733cce985cac05604
Abdullah Furkan Titiz <65949007+afurkantitiz@users.noreply.github.com>
2021-09-25 14:49:18 +0300

    Send api request by searchview

--- app/src/main/java/com/afurkantitiz/newsapp/ui/news/NewsFragment.kt
+++ app/src/main/java/com/afurkantitiz/newsapp/ui/news/NewsFragment.kt
@@ -6,6 +6,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.appcompat.widget.SearchView
 import androidx.fragment.app.viewModels
 import androidx.recyclerview.widget.DividerItemDecoration
 import androidx.recyclerview.widget.LinearLayoutManager
@@ -36,7 +37,21 @@ class NewsFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         initViews()
-        getNewsByQuery()
+        getNewsByQuery("besiktas")
+        searchViewListener()
+    }
+
+    private fun searchViewListener() {
+        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
+            override fun onQueryTextSubmit(query: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(newText: String?): Boolean {
+                if (newText!!.isNotEmpty()) getNewsByQuery(newText) else getNewsByQuery("besiktas")
+                return true
+            }
+        })
     }
 
     private fun initViews() {
@@ -45,8 +60,8 @@ class NewsFragment : Fragment() {
         binding.newsRecyclerView.adapter = newsAdapter
     }
 
-    private fun getNewsByQuery() {
-        viewModel.getNewsByQuery("bitcoin").observe(viewLifecycleOwner, { response->
+    private fun getNewsByQuery(query: String) {
+        viewModel.getNewsByQuery(query).observe(viewLifecycleOwner, { response->
             when (response.status) {
                 Resource.Status.LOADING -> {
                     binding.newsRecyclerView.gone()
