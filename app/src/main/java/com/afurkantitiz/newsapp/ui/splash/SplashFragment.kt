7a2d5d4f2a0b991a6108a1af974bdd544f0f1991
Abdullah Furkan Titiz <65949007+afurkantitiz@users.noreply.github.com>
2021-09-26 21:51:06 +0300

    Added lottie animation for splash screen

---
+++ app/src/main/java/com/afurkantitiz/newsapp/ui/splash/SplashFragment.kt
@@ -0,0 +1,40 @@
+package com.afurkantitiz.newsapp.ui.splash
+
+import android.animation.Animator
+import android.os.Bundle
+import android.util.Log
+import android.view.View
+import androidx.navigation.fragment.findNavController
+import com.afurkantitiz.newsapp.R
+import com.afurkantitiz.newsapp.base.BaseFragment
+import com.afurkantitiz.newsapp.databinding.FragmentSplashBinding
+
+class SplashFragment : BaseFragment<FragmentSplashBinding>(FragmentSplashBinding::inflate) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        lottieListener()
+    }
+
+    private fun lottieListener() {
+        binding.lottieAnimation.addAnimatorListener(object: Animator.AnimatorListener{
+            override fun onAnimationStart(animation: Animator?) {
+                Log.v("Animation","Started")
+            }
+
+            override fun onAnimationEnd(animation: Animator?) {
+                findNavController().navigate(R.id.action_splashFragment_to_newsFragment)
+            }
+
+            override fun onAnimationCancel(animation: Animator?) {
+                Log.v("Animation","Canceled")
+            }
+
+            override fun onAnimationRepeat(animation: Animator?) {
+                Log.v("Animation","Repeated")
+            }
+        })
+    }
+
+    override fun isNavigationBarVisible(): Boolean = false
+}
